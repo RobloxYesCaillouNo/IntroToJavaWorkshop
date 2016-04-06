c7085d5e44fe28f5f66969dd114ddd63aad8aac7
Nicholas Contreras <nicholasc2000@yahoo.com>
2016-04-06 15:01:12 -0700

    ask deez

---
+++ src/day3/Noname.java
@@ -0,0 +1,25 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class Noname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("greetings human");
+	JOptionPane.showMessageDialog(null,"hey there");
+	JOptionPane.showMessageDialog(null, "I like your shirt");
+	JOptionPane.showMessageDialog(null,"I like you a lot");
+	JOptionPane.showMessageDialog(null,"You did such a great job today");
+	JOptionPane.showMessageDialog(null,"Awesome Job");
+	JOptionPane.showInputDialog("Where Did you come from?");
+	int num=8;
+	String word="Blah!";
+	JOptionPane.showMessageDialog(null,word);
+	String name=JOptionPane.showInputDialog("What is your name?");
+	JOptionPane.showMessageDialog(null,"Hi there "+ name);
+	
+	}
+
+
+}
---
+++ src/day3/Sucrityquestionz.java
@@ -0,0 +1,26 @@
+package day3;
+
+import javax.print.attribute.standard.JobKOctetsProcessed;
+import javax.swing.JOptionPane;
+
+public class Sucrityquestionz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String word=JOptionPane.showInputDialog("What was the first model of your car?  ");
+	JOptionPane.showMessageDialog(null,"My first model of a car was ****");
+	JOptionPane.showInputDialog(null,"Phone Number");
+	JOptionPane.showInputDialog("Verify Phone Number");
+	JOptionPane.showInputDialog("Licence Plate No.");	
+	JOptionPane.showInputDialog("Verify Licence Plate No.");
+	JOptionPane.showMessageDialog(null,"I know your street adress");
+	JOptionPane.showInputDialog("Please enter your credit/debit card no.");
+	JOptionPane.showInputDialog("Verify Debit/Credit card No.");
+	JOptionPane.showInputDialog("First Name");
+	JOptionPane.showInputDialog("Last Name");
+	JOptionPane.showInputDialog("Enter Your Birthdate");
+	JOptionPane.showMessageDialog(null,"Success!");
+	JOptionPane.showMessageDialog(null,"Warning. This program can track you to the furthers area!! Heheheh");  
+	}	
+
+}
